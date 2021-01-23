e955c0f39a3df15ed9b279d42c1ddb32810d09a0
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2021-01-23 14:26:31 +0300

    ConfigServer, EurekaServer, ApiGateway, UaaServer added

---
+++ uaa-server/src/main/java/tr/kafein/com/uaaserver/UaaServerApplication.java
@@ -0,0 +1,15 @@
+package tr.kafein.com.uaaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UaaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaaServerApplication.class, args);
+	}
+
+}
---
+++ uaa-server/src/test/java/tr/kafein/com/uaaserver/UaaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.kafein.com.uaaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
