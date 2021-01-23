109f0e25d231100ba729495ef16354ae526bcdf1
Mahmut <mahmut@codefirst.io>
2021-01-23 18:09:13 +0300

    uaa user-service feign client updated

--- uaa-server/src/main/java/tr/kafein/com/uaaserver/accessor/UserServiceAccessor.java
+++ uaa-server/src/main/java/tr/kafein/com/uaaserver/accessor/UserServiceAccessor.java
@@ -1,6 +1,7 @@
 package tr.kafein.com.uaaserver.accessor;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import tr.kafein.com.uaaserver.dto.UserDto;
@@ -8,6 +9,6 @@ import tr.kafein.com.uaaserver.dto.UserDto;
 @FeignClient(value = "user-service", url = "http://localhost:8766/")
 public interface UserServiceAccessor  {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/get-one", produces = "application/json")
-    UserDto getOne();
+    @RequestMapping(method = RequestMethod.GET, value = "/username/{username}", produces = "application/json")
+    UserDto findByUsername(@PathVariable String username);
 }
