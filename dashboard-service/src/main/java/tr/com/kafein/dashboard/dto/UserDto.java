98b152e694d147c7040e86e2169861c83c76fced
Mahmut <mahmut@codefirst.io>
2021-01-24 10:36:17 +0300

    dashboardservice useraccessor updated

---
+++ dashboard-service/src/main/java/tr/com/kafein/dashboard/dto/UserDto.java
@@ -0,0 +1,31 @@
+package tr.com.kafein.dashboard.dto;
+
+public class UserDto {
+    public Long id;
+    public String name;
+    public String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
