3ae03c5fb4e6bdf70408ef37e6e000dfe1ce8e3b
Bilal <kocogluubilal@gmail.com>
2021-02-03 01:37:43 +0300

    pom.xml review all services

--- wall-service/src/main/java/tr/com/kafein/wall/security/FeignClientInterceptor.java
+++ wall-service/src/main/java/tr/com/kafein/wall/security/FeignClientInterceptor.java
@@ -5,7 +5,6 @@ import feign.RequestTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
